8a4fc3efbfa0c964d3afac90b8ec7099b0cc451f
Giovanni Sena <@giovanni.sena>
2022-01-05 23:34:37 -0300

    Paginate with react-paginate

--- src/pages/characters.js
+++ src/pages/characters.js
@@ -7,55 +7,81 @@ import Link, { LinkProps } from "next/link";
 
 
 import Router from 'next/router'
+import { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 
 
 
 export default function Characters({characters, page}) {
   
-    return (
+  const [items, setitems] = useState([]);
+  const [infos, setInfos] = useState();
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(()=>{
+    setitems(characters.results);
+    setInfos(characters.info);
+  },[characters.results, characters.info, currentPage])
+
+  const PER_PAGE = 20;
+  const offset = currentPage * PER_PAGE;
+  const pageCount = Math.ceil(infos?.count / PER_PAGE);
+
+  const handlePageClick = ({ selected: selectedPage }) => {
+    setCurrentPage(selectedPage);
+    Router.push(`?page=${page = selectedPage + 1}`);
+  };
+
+
+
+  return (
     <>  
     <div className={styles.title}>
       <h1>All Characters avaliable in API!</h1>
     </div>   
-
     <div className={styles.containerMain}>
-        {characters.results.map((item, key) => {
-            return (
-              <section key={item.id} className={styles.cardContainer}>
-                <div className={styles.card}>
-                  <div className={styles.cardContent}>
-                    <h2>{item.name}</h2>
-                    <span className={styles.status}>
-                      <img src="/images/lens.svg" alt="circle" className={item.status == 'Dead' ? styles.dead : item.status === 'unknown' ? styles.unknown : null}></img>
-                      {item.status}
-                    </span>
-                    <p>{item.species}</p>
-                    <p>{item.gender}</p>
-                    <p>Origin: {item.origin.name}</p>
+            {items.slice(offset, offset+PER_PAGE).map((item)=>(
+                 
+                 <section key={item.id} className={styles.cardContainer}>
+                 <div className={styles.card}>
+                   <div className={styles.cardContent}>
+                     <h2>{item.name}</h2>
+                     <span className={styles.status}>
+                       <img src="/images/lens.svg" alt="circle" className={item.status == 'Dead' ? styles.dead : item.status === 'unknown' ? styles.unknown : null}></img>
+                       {item.status}
+                     </span>
+                     <p>{item.species}</p>
+                     <p>{item.gender}</p>
+                     <p>Origin: {item.origin.name}</p>
+ 
+                     <Link href={`/character/${item.id}`}>
+                       <a className={styles.link_slug}>Show character</a>
+                     </Link>
+                   </div>
+                   <div className={styles.cardImage}>
+                     <img src={item.image}></img>
+                   </div>
+                 </div>
+               </section>
+               
+            ))}
+      </div>
 
-                    <Link href={`/character/${item.id}`}>
-                      <a className={styles.link_slug}>Show character</a>
-                    </Link>
-                  </div>
-                  <div className={styles.cardImage}>
-                    <img src={item.image}></img>
-                  </div>
-                </div>
-              </section>
-        )
-        })}
-
-
-        <button className={styles.paginate_preview} onClick={() => Router.push(`?page=${page - 1}`)}>
-          PREVIEW
-        </button>
-
-        <button className={styles.paginate_next} onClick={() => Router.push(`?page=${page + 1}`)}>
-          NEXT
-        </button>
-      
-        
-        </div>
+      <section className={styles.paginate_container}>
+       <div className={styles.paginate}>
+       <ReactPaginate
+                previousLabel={"Anterior"}
+                nextLabel={"Próximo"}
+                pageCount={pageCount}
+                onPageChange={handlePageClick}
+                containerClassName={"pagination"}
+                previousLinkClassName={"pagination__link"}
+                nextLinkClassName={"pagination__link"}
+                disabledClassName={"pagination__link--disabled"}
+                activeClassName={"pagination__link--active"}
+              />
+       </div>
+      </section>
 
     </>
            
